df17fda57ca072c4a7d89b6aa5c5a6f87580cae8
Keith Yager <kyager@gmail.com>
2024-07-08 22:57:06 -0400

    Add logic

--- src/BinaryToText/Program.cs
+++ src/BinaryToText/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            var firstByte = args[0];
+            int result = 0;
+
+            for (int i = 0; i < firstByte.Length; i++)
+            {
+                if (firstByte[i] == '1') {
+                    result += (int)Math.Pow(2, firstByte.Length - i - 1);
+                }
+            }
+
+            System.Console.WriteLine(result);
+        }
+        else
+        {
+            System.Console.WriteLine("No input detected!");
+        }
+    }
+}
