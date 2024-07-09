5521b6d03b5d7fd6c1e38a3a4eb44b1e5b5601a0
Keith Yager <kyager@gmail.com>
2024-07-08 23:05:50 -0400

    Add logic

--- src/BinaryToText/Program.cs
+++ src/BinaryToText/Program.cs
@@ -5,16 +5,16 @@
         if (args.Length > 0)
         {
             var firstByte = args[0];
-            int result = 0;
+            int unicode = 0;
 
             for (int i = 0; i < firstByte.Length; i++)
             {
                 if (firstByte[i] == '1') {
-                    result += (int)Math.Pow(2, firstByte.Length - i - 1);
+                    unicode += (int)Math.Pow(2, firstByte.Length - i - 1);
                 }
             }
 
-            System.Console.WriteLine(result);
+            System.Console.WriteLine((char) unicode);
         }
         else
         {
